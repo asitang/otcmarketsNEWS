7e9de879b23b5a47d87b04174cb1dc4ee5b7505f
Asitang Mishra <asitang@gmail.com>
2015-07-22 15:33:58 -0400

    now the code goes to each link in all pages

--- Extractone.java
+++ Extractone.java
@@ -20,58 +20,102 @@ public class Extractone  {
         
 
         
-        driver.get("http://www.otcmarkets.com/stock/PZOO/news");
+        driver.get("http://www.otcmarkets.com/stock/GOOG/news");
+        
+        Thread.sleep(3000);
         
         boolean next=true;
       
-        //while(next){
+        while(next){
+            next=false;
         
-        WebElement table = driver.findElement(By.id("outsideNewsTable"));
-       
-       List<WebElement> tbodies= table.findElements( By.tagName("tbody"));
 
-       
-        for(WebElement ele:tbodies){
-        	
-        	try{
-        		ele.findElement(By.tagName("a"));
-        	}
-        	catch(Exception e){
-        		continue;
-        	}
-        	
-        		
-        	WebElement atag=ele.findElement(By.tagName("a"));
-        
-        	//System.out.println(atag.getText());
-        	atag.click();
-        	
-        	 Thread.sleep(3000);
-        	// System.out.println(driver.getPageSource());
-        	WebElement newscontainer = driver.findElement(By.className("newsContainer"));
-        	System.out.println(newscontainer.getText());
-        	WebElement newsback = driver.findElement(By.className("newsBack"));
-        	WebElement back =newsback.findElement(By.tagName("a"));
-        	back.click();
-        	Thread.sleep(3000);
-        	break;
+            WebElement table = driver.findElement(By.id("outsideNewsTable"));
+            
+            List<WebElement> tbodies= table.findElements( By.tagName("tbody"));
+           
+            
+        for(int i=0;i<tbodies.size();i++){
+            
+            try{
+                tbodies.get(i).findElement(By.tagName("a"));
+            }
+            catch(Exception e){
+                continue;
+            }
+            
+            List<WebElement> trs= tbodies.get(i).findElements( By.tagName("tr"));
+            
+            
+            for(int j=0;j<trs.size();j++){
+                
+                
+                WebElement atag=trs.get(j).findElement(By.tagName("a"));
+                if(atag.getAttribute("href").equals("javascript:void(null);")){
+            
+                
+                atag.click();
+                
+                 Thread.sleep(3000);
+                
+                // System.out.println(driver.getPageSource());
+                WebElement newscontainer = driver.findElement(By.className("newsContainer"));
+                System.out.println(newscontainer.getText());
+                WebElement newsback = driver.findElement(By.className("newsBack"));
+                WebElement back =newsback.findElement(By.tagName("a"));
+                back.click();
+                 Thread.sleep(3000);    
+                 
+                 //refreshing the holders as the page was messed with
+                 
+                  table = driver.findElement(By.id("outsideNewsTable"));
+                 
+                  tbodies= table.findElements( By.tagName("tbody"));
+                  
+                  trs= tbodies.get(i).findElements( By.tagName("tr"));
+                  
+                  //refresh ends
+                }
+                else{
+                    System.out.println(atag.getAttribute("href"));
+                }
+                
+            }
+                
+            
+            
        }
         
+        //check for the next button, if found then click set the flag to true
         
-        //List<WebElement> pagination=driver.findElements(By.className("pageList"));
-        //for
+        WebElement pagination=driver.findElement(By.id("outsideNewsTable")).findElement(By.className("pageList"));
+        List<WebElement> paginationli=pagination.findElements(By.tagName("li"));
+      
         
-        //} 
+        for(WebElement ele:paginationli){
+            
+            
+            try{
+            ele.findElement(By.tagName("a")).getText();
+            }
+            catch(Exception e){
+                continue;
+            }
+            
+            if(ele.findElement(By.tagName("a")).getText().equals("next >")){
+                //System.out.println("HI");
+                WebElement nextpage= ele.findElement(By.tagName("a"));
+                nextpage.click();
+                next=true;
+                Thread.sleep(3000);
+                
+            }
+            
+        }
         
-/*        List<WebElement> tabletr= table.findElements(By.xpath(".//tbody/tr"));
+              } 
         
-for(WebElement ele:tabletr){
-	for (WebElement elein:ele.findElements(By.xpath(".//td"))){
-		if (elein.getAttribute("class")==null||elein.getAttribute("class")==""){
-			elein.
-		}
-	}
-	}*/
+
 
         driver.quit();
     }
